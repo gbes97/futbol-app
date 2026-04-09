7c5560cd473eea0a469a843879af0a8a12dcd7e4
gbes97 <gibran.es97@gmail.com>
2026-04-09 13:06:08 -0600

    feat: contador

--- src/App.jsx
+++ src/App.jsx
@@ -4,3 +4,36 @@ function App() {
   )
 }
 export default App;
+
+import { useState } from 'react';
+function Goles() {
+  const [goles, setGoles] = useState();
+}
+
+let goles = 0;
+
+function sumarGol() {
+ goles++;
+   console.log(`Gol! Total: ${goles}`)
+ }
+
+function restarGol() {
+    if (goles > 0) {
+        goles--;
+        console.log(`Se anuló un gol. Total: ${goles}`);
+    } else {
+        console.log("No se puede restar, no hay goles.");
+    }
+}
+const botonSumar = document.querySelector('.sumar');
+botonSumar.addEventListener('click', () => {
+  sumarGol();
+})
+
+const botonRestar = document.querySelector('.restar');
+botonRestar.addEventListener('click', () => {
+  restarGol();
+})
+
+
+
